f74b9a9072d8a650eca6450a2dc437408d2a8bf7
easechen <956902354@qq.com>
2021-06-15 18:30:30 +0800

    update

--- src/login.py
+++ src/login.py
@@ -5,6 +5,8 @@ from lxml import etree
 userName = '192842***' # 用户名
 passwd = 'abc***' # 密码
 net = '移动' # 类型，移动，联通，电信
+
+passwd_1 = ''#教学楼校园网密码（身份证后七位的前六位）
 # ------------------------------------
 
 # return start urls
@@ -47,7 +49,7 @@ def createInfo(location,userName, passwd, net, r):
     # 东综
     elif (location == '教学楼'):
         head = {'Host': gateway_host_ip,'Proxy-Connection': 'keep-alive','Content-Length': '600','Cache-Control': 'max-age=0','Upgrade-Insecure-Requests': '1','Origin': gateway_host,'Content-Type': 'application/x-www-form-urlencoded','User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36','Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9','Referer': login_url,'Accept-Encoding': 'gzip, deflate','Accept-Language': 'zh-CN,zh;q=0.9','Cookie': cookie}
-        body = {'wlanuserip': wlanuserip,'wlanacname': wlanacname,'chal_id':'' ,'chal_vector':'' ,'auth_type': 'PAP','seq_id':'' ,'req_id': '','wlanacIp': wlanacIp,'ssid': '','vlan':'' ,'mac': '','message':'' ,'bank_acct': '','isCookies':'' ,'version': '0','authkey': '88----89','url': '','usertime': '0','listpasscode': '0','listgetpass': '0','getpasstype': '0','randstr': '8587','domain': '','isRadiusProxy': 'false','usertype': '0','isHaveNotice': '0','times': '12','weizhi': '0','smsid': '','freeuser':'' ,'freepasswd': '','listwxauth': '0','templatetype': '1','tname': 'shida_pc_portal','logintype': '0','act':'' ,'is189': 'false','terminalType':'' ,'checkterminal': 'true','portalpageid': '101','listfreeauth': '0','viewlogin': '1','userid': '1928424171', 'authGroupId':'' , 'useridtemp': '1928424171', 'passwd': '207211' } 
+        body = {'wlanuserip': wlanuserip,'wlanacname': wlanacname,'chal_id':'' ,'chal_vector':'' ,'auth_type': 'PAP','seq_id':'' ,'req_id': '','wlanacIp': wlanacIp,'ssid': '','vlan':'' ,'mac': '','message':'' ,'bank_acct': '','isCookies':'' ,'version': '0','authkey': '88----89','url': '','usertime': '0','listpasscode': '0','listgetpass': '0','getpasstype': '0','randstr': '8587','domain': '','isRadiusProxy': 'false','usertype': '0','isHaveNotice': '0','times': '12','weizhi': '0','smsid': '','freeuser':'' ,'freepasswd': '','listwxauth': '0','templatetype': '1','tname': 'shida_pc_portal','logintype': '0','act':'' ,'is189': 'false','terminalType':'' ,'checkterminal': 'true','portalpageid': '101','listfreeauth': '0','viewlogin': '1','userid': '1928424171', 'authGroupId':'' , 'useridtemp': userName, 'passwd': passwd_1 } 
         #raw_body = "wlanuserip=%s&wlanacname=%s&chal_id=&chal_vector=&auth_type=PAP&seq_id=&req_id=&wlanacIp=210.42.255.60&ssid=&vlan=&mac=&message=&bank_acct=&isCookies=&version=0&authkey=88----89&url=&usertime=0&listpasscode=0&listgetpass=0&getpasstype=0&randstr=2934&domain=&isRadiusProxy=false&usertype=0&isHaveNotice=0&times=12&weizhi=0&smsid=&freeuser=&freepasswd=&listwxauth=0&templatetype=1&tname=shida_pc_portal&logintype=0&act=&is189=false&terminalType=&checkterminal=true&portalpageid=101&listfreeauth=0&viewlogin=1&userid=601nb&authGroupId=&useridtemp=%s&passwd=%s" % (wlanuserip, wlanacname, userName, passwd)
     return head, body
 
